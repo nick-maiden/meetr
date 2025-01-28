4f0402495b5178a04123b1fb346ca9e8e82fabdb
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2025-01-28 17:23:55 +1100

    feat: start implementing simplified version of backend routes

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,75 +1,117 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
 
 module Main (main) where
 
 import Web.Scotty
-import Database.MongoDB
-import Control.Monad.Trans (liftIO)
+import Data.Acid
+import Data.SafeCopy
+import Control.Monad.Reader
+import qualified Control.Monad.State as S
 import qualified Data.Aeson as A
 import Network.Wai.Middleware.Cors
 import Network.Wai.Middleware.RequestLogger
 import Network.HTTP.Types.Status
+import Data.Typeable
 import GHC.Generics
 import qualified Data.Text as T
-import Control.Exception (SomeException)
+import qualified Data.Map as Map
+import Control.Lens hiding ((.=))
+import Data.Text.Lazy (pack)
 
--- Data type for items
-data Item = Item {
-    name :: T.Text,
-    description :: T.Text
-} deriving (Show, Generic)
+-- Data Types
+data User = User {
+  id :: Int,
+  name :: T.Text
+} deriving (Show, Typeable, Generic)
 
-instance A.ToJSON Item
-instance A.FromJSON Item
+instance A.ToJSON User
+instance A.FromJSON User
+$(deriveSafeCopy 0 'base ''User)
 
--- MongoDB configuration
-type MongoAction = Action IO
+data Event = Event {
+  name :: T.Text,
+  users :: [User],
+  startTime :: T.Text,
+  endTime :: T.Text
+} deriving (Show, Typeable, Generic)
 
-runMongo :: MongoAction a -> IO a
-runMongo action = do
-    pipe <- connect (host "127.0.0.1")
-    access pipe master "test" action
+instance A.ToJSON Event
+instance A.FromJSON Event
+$(deriveSafeCopy 0 'base ''Event)
 
--- Database operations
-getItems :: MongoAction [Item]
-getItems = do
-    docs <- find (select [] "items") >>= rest
-    return $ map (\ doc -> Item 
-        { name = at "name" doc
-        , description = at "description" doc 
-        }) docs
+-- State container
+data EventDB = EventDB {
+    _events :: Map.Map Int Event,
+    _nextId :: Int
+} deriving (Typeable)
 
-insertItem :: Item -> MongoAction A.Value
-insertItem item = do
-    _ <- insert "items" [ "name" =: name item
-                       , "description" =: description item
-                       ]
-    return $ A.Object mempty
+$(makeLenses ''EventDB)
+
+-- Initial state
+initialEventDBState :: EventDB
+initialEventDBState = EventDB Map.empty 0
+
+$(deriveSafeCopy 0 'base ''EventDB)
+
+-- Acid-State queries and updates
+getAllEvents :: Query EventDB [Event]
+getAllEvents = asks (Map.elems . _events)
+
+getEvent :: Int -> Query EventDB (Maybe Event)
+getEvent eventId = do
+  db <- ask
+  return (Map.lookup eventId (_events db))
+
+insertNewEvent :: Event -> Update EventDB Int
+insertNewEvent event = do
+    i <- use nextId
+    events %= Map.insert i event
+    nextId += 1
+    return i
+
+$(makeAcidic ''EventDB ['getAllEvents, 'insertNewEvent, 'getEvent])
 
 -- Main application
 main :: IO ()
-main = scotty 3000 $ do
-    -- Middleware
-    middleware simpleCors
-    middleware logStdoutDev
+main = do
+    -- Initialize Acid-State
+    acid <- openLocalState initialEventDBState
 
-    -- Routes
-    get "/items" $ do
-        items <- liftAndCatchIO $ runMongo getItems
-        json items
-    
-    post "/items" $ do
-        item <- jsonData
-        result <- liftAndCatchIO $ runMongo (insertItem item)
-        status status201
-        json result
-    
-    notFound $ do
-        status status404
-        json $ A.object ["error" A..= ("Route not found" :: String)]
+    -- Start Scotty
+    scotty 3000 $ do
+        -- Middleware
+        middleware simpleCors
+        middleware logStdoutDev
 
-    -- Error handling
-    -- defaultHandler $ do
-    --     status status500
+        -- Routes
+        get "/events" $ do
+            events <- liftAndCatchIO $ query acid GetAllEvents
+            json events
+
+        get "/events/:id" $ do
+          eventId <- param "id"
+          event <- liftAndCatchIO $ query acid (GetEvent (read eventId :: Int))
+          json event
+
+        post "/events" $ do
+            event <- jsonData
+            eventId <- liftAndCatchIO $ update acid (InsertNewEvent event)
+            status status201
+            json $ A.object ["id" A..= eventId]
+
+        notFound $ do
+            status status404
+            json $ A.object ["error" A..= ("Route not found" :: String)]
+
+        -- Error handling
+        -- defaultHandler $ \err -> do
+        --     status status500
+        --     json $ A.object ["error" A..= pack (show err)]
+
+    -- Close Acid-State on shutdown
+    closeAcidState acid
