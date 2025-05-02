16478712f17c4840e0fab8260bd320896dc8489e
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2025-05-02 19:18:20 +1000

    feat: introduce containerisation into backend build and deploy

--- backend/src/Config.hs
+++ backend/src/Config.hs
@@ -1,53 +1,36 @@
 module Config
-  ( defaultConfig
-  , readConfig
-  ) where
+  (readConfig) where
 
-import Types (Config(..), LogLevel(..))
+import Types (Config(..))
 import System.Environment (lookupEnv)
-import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
-
-defaultConfig :: Config
-defaultConfig = Config
-  { port = 8080
-  , host = "127.0.0.1"
-  , allowedOrigins = ["http://127.0.0.1:5173"]
-  , logLevel = Production
-  }
+import System.Exit (die)
 
 -- Read configuration from environment variables
 readConfig :: IO Config
 readConfig = do
-  port <- lookupEnvInt "PORT" (port defaultConfig)
-  host <- lookupEnvString "HOST" (host defaultConfig)
-  origins <- lookupEnvStringList "ALLOWED_ORIGINS" (allowedOrigins defaultConfig)
-  logLevel <- lookupEnvEnum "LOG_LEVEL" (logLevel defaultConfig)
+  portStr <- lookupEnv "PORT" >>= maybe (die "PORT env var required") return
+  hostStr <- lookupEnv "HOST" >>= maybe (die "HOST env var required") return
+  originsStr <- lookupEnv "ALLOWED_ORIGINS" >>= maybe (die "ALLOWED_ORIGINS env var required") return
+  logLevelStr <- lookupEnv "LOG_LEVEL" >>= maybe (die "LOG_LEVEL env var required") return
+
+  port <- case readMaybe portStr of
+            Just p  -> return p
+            Nothing -> die "PORT must be a valid integer"
+
+  let origins = splitOn ',' originsStr
+
+  logLevel <- case readMaybe logLevelStr of
+               Just l  -> return l
+               Nothing -> die "LOG_LEVEL must be a valid enum value"
 
   return Config
     { port = port
-    , host = host
+    , host = hostStr
     , allowedOrigins = origins
     , logLevel = logLevel
     }
   where
-    lookupEnvInt name defaultVal = do
-      val <- lookupEnv name
-      return $ fromMaybe defaultVal (val >>= readMaybe)
-
-    lookupEnvString name defaultVal = do
-      val <- lookupEnv name
-      return $ fromMaybe defaultVal val
-
-    lookupEnvStringList name defaultVal = do
-      val <- lookupEnv name
-      return $ maybe defaultVal (splitOn ',') val
-
-    lookupEnvEnum name defaultVal = do
-      val <- lookupEnv name
-      return $ fromMaybe defaultVal (val >>= readMaybe)
-
-    splitOn :: Char -> String -> [String]
     splitOn c s = case break (== c) s of
       (x, []) -> [x]
       (x, _:xs) -> x : splitOn c xs
