1361659764cbf75bea3111104fb04b855dc7268b
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2025-06-25 14:52:02 +1000

    refactor: style corrections

--- frontend/src/app/PageList.tsx
+++ frontend/src/app/PageList.tsx
@@ -1,16 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import CreateEvent from 'pages/CreateEvent/CreateEvent';
-import Event from 'pages/Event/Event';
-import NotFoundPage from 'pages/NotFoundPage';
+import { Route, Routes } from "react-router-dom";
+import CreateEvent from "pages/CreateEvent/CreateEvent";
+import Event from "pages/Event/Event";
+import NotFoundPage from "pages/NotFoundPage";
 
 const PageList = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<CreateEvent />} />
-        <Route path='/events/:eventId' element={<Event />} />
-        <Route path='/404' element={<NotFoundPage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/" element={<CreateEvent />} />
+        <Route path="/events/:eventId" element={<Event />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
--- frontend/src/pages/CreateEvent/utils.ts
+++ frontend/src/pages/CreateEvent/utils.ts
@@ -1,18 +1,19 @@
 import { ADJECTIVES, NOUNS, EVENT_TYPES } from "./constants";
 
 export const getHour = (timeStr: string): number => {
-  const [hourStr, period] = timeStr.split(' ');
+  const [hourStr, period] = timeStr.split(" ");
   let hour = parseInt(hourStr);
-  if (period === 'pm' && hour !== 12) hour += 12;
-  if (period === 'am' && hour === 12) hour = 0;
+  if (period === "pm" && hour !== 12) hour += 12;
+  if (period === "am" && hour === 12) hour = 0;
   return hour;
 };
 
 export const convertTo24Hour = (timeStr: string): string => {
-  return `${getHour(timeStr).toString().padStart(2, '0')}:00`;
+  return `${getHour(timeStr).toString().padStart(2, "0")}:00`;
 };
 
-const getRand = <T>(arr: readonly T[]): T => arr[Math.floor(Math.random() * arr.length)];
+const getRand = <T>(arr: readonly T[]): T =>
+  arr[Math.floor(Math.random() * arr.length)];
 
-export const generateName = (): string => `${getRand(ADJECTIVES)} ${getRand(NOUNS)} ${getRand(EVENT_TYPES)}`;
-
+export const generateName = (): string =>
+  `${getRand(ADJECTIVES)} ${getRand(NOUNS)} ${getRand(EVENT_TYPES)}`;
